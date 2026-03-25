d0e93af5e592f0e66f4bf68c5515a798dc5a1eb0
Hend Soliman <hendsoliman5@gmail.com>
2026-03-25 15:14:36 +0200

    initial commit

---
+++ src/main/java/com/aboali/aboali_pos_backend/AboaliPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aboali.aboali_pos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboaliPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboaliPosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aboali/aboali_pos_backend/AboaliPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboali.aboali_pos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboaliPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
